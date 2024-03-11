41171b4993ffe067d07c232825b3a21a653a521c
Colin <1454734992@qq.com>
2024-03-11 22:33:46 +0800

    Hey0224 111

---
+++ demo20240222/src/main/java/com/example/demo20240222/Demo20240222Application.java
@@ -0,0 +1,14 @@
+package com.example.demo20240222;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo20240222Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo20240222Application.class, args);
+        System.out.println("Hey");
+    }
+
+}
---
+++ demo20240222/src/test/java/com/example/demo20240222/Demo20240222ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo20240222;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo20240222ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
